298ac50fffea20da91b8947462fa5dd196faa8ff
Marcell Alves <marcell.alves@gmail.com>
2020-04-08 21:45:40 -0300

    feat: add cycle detection method

--- com/codewithmosh/Main.java
+++ com/codewithmosh/Main.java
@@ -3,16 +3,17 @@ package com.codewithmosh;
 public class Main {
     public static void main(String[] names) {
       var graph = new Graph();
-      graph.addNode("X");
       graph.addNode("A");
       graph.addNode("B");
-      graph.addNode("P");
+      graph.addNode("C");
+      graph.addNode("D");
 
-      graph.addEdge("X", "A");
-      graph.addEdge("X", "B");
-      graph.addEdge("A", "P");
-      graph.addEdge("B", "P");
-      var list = graph.topologicalSort();
-      System.out.println(list);
+      graph.addEdge("A", "B");
+      graph.addEdge("B", "C");
+      graph.addEdge("A", "C");
+      graph.addEdge("D", "A");
+      //var list = graph.topologicalSort();
+      //System.out.println(list);
+      System.out.println(graph.hasCycle());
     }
 }
