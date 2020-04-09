7cd153295ad452c25faa9c00a7542dd9025b414e
Marcell Alves <marcell.alves@gmail.com>
2020-04-09 19:33:35 -0300

    feat: add Dijkstra's shortest path

--- com/codewithmosh/WeightedGraph.java
+++ com/codewithmosh/WeightedGraph.java
@@ -1,9 +1,13 @@
 package com.codewithmosh;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class WeightedGraph {
     private class Node {
@@ -65,6 +69,51 @@ public class WeightedGraph {
 
         nodeFrom.addEdge(nodeTo, weight);
         nodeTo.addEdge(nodeFrom, weight);
+    }
+
+    private class NodeEntry {
+        private Node node;
+        private int priority;
+
+        public NodeEntry(Node node, int priority) {
+            this.node = node;
+            this.priority = priority;
+        }
+    }
+
+    public int getShortestPath(String from, String to) {
+        var fromNode = nodes.get(from);
+
+        Map<Node, Integer> distances = new HashMap<>();
+        for (var node : nodes.values())
+            distances.put(node, Integer.MAX_VALUE);
+        distances.replace(fromNode, 0);
+
+        Set<Node> visited = new HashSet<>();
+
+        PriorityQueue<NodeEntry> queue = new PriorityQueue<>(
+            Comparator.comparingInt(ne -> ne.priority)
+        );
+        queue.add(new NodeEntry(fromNode, 0));
+
+        while (!queue.isEmpty()) {
+            var current = queue.remove().node;
+            visited.add(current);
+
+            for (var edge : current.getEdges()) {
+                if (visited.contains(edge.to))
+                    continue;
+
+                var newDistance = distances.get(current) + edge.weight;
+                if (newDistance < distances.get(edge.to)) {
+                    distances.replace(edge.to, newDistance);
+                    queue.add(new NodeEntry(edge.to, newDistance));
+                }
+
+            }
+        }
+
+        return distances.get(nodes.get(to));
     }
 
     public void print() {
